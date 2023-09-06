90b528e29748c85eb626406bfd80a57e9a388188
Flavien <flavien.louvet2004@gmail.com>
2023-09-06 13:59:23 +0200

    Choix du joueur

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function paragraphe(p){
-    global.innerHTML += "<h2> " + p + " </h2>"
+
+let rock = document.getElementById('rock')
+let paper = document.getElementById('paper')
+let scissors = document.getElementById('scissors')
+
+rock.addEventListener('click', () =>{
+    result("Rock")
+})
+paper.addEventListener('click', () =>{
+    result("Paper")
+})
+scissors.addEventListener('click', () =>{
+    result("Scissors")
+})
+
+function result(pick){
+    let result = document.getElementById('result')
+    result.innerHTML = "<h1> Player : " + pick + "</h1>"
+
 }
