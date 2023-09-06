a9d3cac771d27be1d1c98bed5e38555d80a20ac4
Flavien <flavien.louvet2004@gmail.com>
2023-09-06 14:08:03 +0200

    Choix du bot

--- script.js
+++ script.js
@@ -13,8 +13,17 @@ scissors.addEventListener('click', () =>{
     result("Scissors")
 })
 
+function random(max) {
+    return Math.floor(Math.random() * max);
+}
+
 function result(pick){
+    //Affiche le choix du joueur
     let result = document.getElementById('result')
     result.innerHTML = "<h1> Player : " + pick + "</h1>"
 
+    //Affiche le choix du bot
+    let picks = ['Rock','Paper','Scissors']
+    let botPick = picks[random(3)]
+    result.innerHTML += "<h1> Bot : " + botPick + "</h1>"
 }
