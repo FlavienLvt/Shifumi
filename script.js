fe507b673c052395f68f40abd4b324ae59f60875
Flavien <flavien.louvet@unicaen.fr>
2023-09-07 11:00:44 +0200

    Version final, refonte du code et js doc

--- script.js
+++ script.js
@@ -1,37 +1,67 @@
 
+let picks = ['Rock','Paper','Scissors']
 let rock = document.getElementById('rock')
 let paper = document.getElementById('paper')
 let scissors = document.getElementById('scissors')
 
 rock.addEventListener('click', () =>{
-    result("Rock")
+    displayGame(picks[0],botRandomPicker())
 })
 paper.addEventListener('click', () =>{
-    result("Paper")
+    displayGame(picks[1],botRandomPicker())
 })
 scissors.addEventListener('click', () =>{
-    result("Scissors")
+    displayGame(picks[2],botRandomPicker())
 })
 
+/**
+ * This function pick a random number
+ * @param {*Range for the random} max 
+ * @returns 
+ */
 function random(max) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max)
 }
 
-function result(pick){
+/**
+ * This function return the random value, that is a pick for the array picks
+ * @returns 
+ */
+function botRandomPicker(){
+    let randomBotPick = picks[random(3)]
+    return randomBotPick
+}
+
+/**
+ * This function is used to diplay element in the html page
+ * @param {*Contain an value from picks array that is used for the player pick} playerPick 
+ * @param {*Contain an value from picks array that is used for the bot pick} botPick 
+ */
+function displayGame(playerPick, botPick){
     //Display player pick
     let gamestatus = document.getElementById('pick')
-    gamestatus.innerHTML = "<h1> Player : " + pick + "</h1>"
+    gamestatus.innerHTML = "<h1> Player : " + playerPick + "</h1>"
 
     //Display bot pick
-    let picks = ['Rock','Paper','Scissors']
-    let botPick = picks[random(3)]
     gamestatus.innerHTML += "<h1> Bot : " + botPick + "</h1>"
 
-    //Game issue
+    //Display game issue
     let result = document.getElementById('result')
-    if(pick = botPick)result.innerHTML += "<p> Tie </p>"
-    if(pick = "Paper" && botPick == "Rock")result.innerHTML ="<p> Win </p>"
-    if(pick = "Scissors" && botPick == "Paper")result.innerHTML ="<p> Win </p>"
-    if(pick = "Rock" && botPick == "Scissors")result.innerHTML ="<p> Win </p>"
-    //else result.innerHTML = "<p> Lose </p>"
+    result.innerHTML = gameIssue(playerPick,botPick)
+}
+
+/**
+ * This function is used to compare values from the player and the bot,
+ * and it tell who win or if it's a tie
+ * @param {*Contain an value from picks array that is used for the player pick} playerPick 
+ * @param {*Contain an value from picks array that is used for the bot pick} botPick 
+ * @returns 
+ */
+function gameIssue(playerPick, botPick){
+    //Game issue
+    if(playerPick == botPick) return "<p> Tie </p>"
+    if(playerPick == picks[1] && botPick == picks[0]) return "<p> Win </p>"
+    if(playerPick == picks[2] && botPick == picks[1]) return "<p> Win </p>"
+    if(playerPick == picks[0] && botPick == picks[2]) return "<p> Win </p>"
+    else return "<p> Lose </p>"
 }
